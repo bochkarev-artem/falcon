c9fc077b052797cc6dd119e0d42d59cc71e125c1
Artem Bochkarev <skivius@yandex.ru>
2016-04-18 17:53:14 +0300

    litres service changed

--- src/AppBundle/Service/LitresService.php
+++ src/AppBundle/Service/LitresService.php
@@ -5,6 +5,7 @@
 
 namespace AppBundle\Service;
 
+use AppBundle\Entity\Book;
 
 /**
  * Class LitresService
@@ -12,5 +13,49 @@ namespace AppBundle\Service;
  */
 class LitresService
 {
-    // SimpleXMLElement
+    CONST DETAILED_DATA_FILE = 'http://www.litres.ru/static/ds/detailed_data.xml.gz';
+
+    /**
+     * @param $file
+     * @return resource|bool
+     */
+    public function getFile($file)
+    {
+        $filename = 'detailed_data.xml.gz';
+        if (file_put_contents($filename, fopen($file, 'r'))) {
+            return gzopen($filename, 'r');
+        }
+
+        return false;
+    }
+
+    /**
+     *
+     */
+    public function getDetailedData()
+    {
+        $file = $this->getFile(self::DETAILED_DATA_FILE);
+
+        if ($file) {
+            $xml = new \SimpleXMLElement($file);
+            $book = new Book();
+            foreach ($xml->{'litres-updates'} as $art) {
+                foreach ($art->attributes() as $name => $attribute) {
+                    $data[$name] = $attribute;
+                }
+                if (!empty($data)) {
+                    $book->setLitresIntId($data['int_id']);
+                    $book->setPrice($data['price']);
+                    $book->setCover($data['cover']);
+                    $book->setIsSale($data['on_sale']);
+                    $book->setFileIntId($data['file_id']);
+                    $book->setType($data['type']);
+                    $book->setIsShowPreview($data['show_preview']);
+                    $book->setIsAllowRead($data['allow_read']);
+                }
+            }
+            gzclose($file);
+        }
+
+    }
 }
