b2981d50fecc5900494196507de84a69beb703af
Artem Bochkarev <skivius@yandex.ru>
2016-12-27 13:24:39 +0200

    fix clearing cache in prod env

--- app/deployment/after_install.sh
+++ app/deployment/after_install.sh
@@ -3,6 +3,7 @@ cd /home/ubuntu/falcon/
 export COMPOSER_HOME=./
 composer install
 php bin/console assetic:dump --env=dev
+php bin/console cache:clear --env=prod --no-debug
 HTTPDUSER=`ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
 sudo setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var
 sudo setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var
