25a9231effb23ec864b5722a2caf692286e61b2b
Artem Bochkarev <skivius@yandex.ru>
2017-01-23 22:22:07 +0200

    sitemap section feature in cli added

--- src/AppBundle/Service/SitemapSubscriber.php
+++ src/AppBundle/Service/SitemapSubscriber.php
@@ -50,24 +50,21 @@ class SitemapSubscriber implements EventSubscriberInterface
      */
     public function generateSitemap(SitemapPopulateEvent $event)
     {
-        $queryBuilders[] = $this->getAuthorQueryBuilder();
-        $queryBuilders[] = $this->getBookQueryBuilder();
-        $queryBuilders[] = $this->getGenreQueryBuilder();
-        $queryBuilders[] = $this->getSequenceQueryBuilder();
-        $queryBuilders[] = $this->getTagQueryBuilder();
+        $queryBuilders = [
+            'authors' => $this->getAuthorQueryBuilder(),
+            'books'   => $this->getBookQueryBuilder(),
+            'genres'  => $this->getGenreQueryBuilder(),
+            'series'  => $this->getSequenceQueryBuilder(),
+            'tags'    => $this->getTagQueryBuilder()
+        ];
 
-        $sections[] = 'authors';
-        $sections[] = 'books';
-        $sections[] = 'genres';
-        $sections[] = 'series';
-        $sections[] = 'tags';
-
-        foreach ($queryBuilders as $queryBuilder) {
-            foreach ($queryBuilder->getQuery()->iterate() as $row) {
-                /** @var EntityInterface $entity */
-                $entity  = array_shift($row);
-                $section = array_shift($sections);
-                $event->getUrlContainer()->addUrl(new UrlConcrete($this->siteUrl . $entity->getPath()), $section);
+        foreach ($queryBuilders as $section => $queryBuilder) {
+            if (is_null($event->getSection()) || $event->getSection() == $section) {
+                foreach ($queryBuilder->getQuery()->iterate() as $row) {
+                    /** @var EntityInterface $entity */
+                    $entity = array_shift($row);
+                    $event->getUrlContainer()->addUrl(new UrlConcrete($this->siteUrl . $entity->getPath()), $section);
+                }
             }
         }
     }
