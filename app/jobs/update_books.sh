7a9906c136d662e612580ea239e228f331883422
Artem Bochkarev <skivius@yandex.ru>
2017-02-17 10:34:55 +0200

    added execute rights for bash script for cron

--- app/jobs/update_books.sh
+++ app/jobs/update_books.sh
@@ -3,6 +3,7 @@ cd /home/ubuntu/falcon/
 php bin/console app:update-litres-data
 php bin/console app:update-book-images
 php bin/console app:update-author-images
+php bin/console app:update-featured-menu
 php bin/console fos:elastica:populate --env=prod --no-debug
 php bin/console presta:sitemaps:dump --gzip --env=prod --no-debug
 php bin/console cache:clear --env=prod --no-debug
