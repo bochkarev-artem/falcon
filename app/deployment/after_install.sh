4421ac8a875da6bbca1b6e8897d2b79030baa21e
Artem Bochkarev <skivius@yandex.ru>
2017-03-14 11:02:21 +0200

    deleted semi-column delimiter in tag list

--- app/deployment/after_install.sh
+++ app/deployment/after_install.sh
@@ -4,6 +4,7 @@ export COMPOSER_HOME=./
 composer install
 php bin/console assetic:dump --env=dev
 php bin/console cache:clear --env=prod --no-debug
+rm -rf ./var/cache/*
 HTTPDUSER=`ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
 sudo setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var web/media
 sudo setfacl -dR -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var web/media
