37a934d7d589cc631bb5a4ef8d4eb192a576f4c2
Artyom Bochkarev <skivius@yandex.ru>
2017-05-03 00:28:49 +0300

    template done for profile

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -3,7 +3,6 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Model\Pagination;
-use AppBundle\Model\QueryParams;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -24,21 +23,24 @@ class UserController extends Controller
 
         $perPage     = $this->getParameter('default_per_page');
         $page        = $request->get('page', 1);
+        $offset      = ($page - 1) * $perPage;
         $pageService = $this->get('book_page_service');
         $bookData    = $pageService->getUserRatings($user->getId());
         $bookIds     = array_keys($bookData);
         $bookRepo    = $this->get('doctrine')->getRepository('AppBundle:Book');
-        $books       = $bookRepo->findBy(['id' => $bookIds], ['updatedOn' => 'DESC']);
+        $books       = $bookRepo->findBy(['id' => $bookIds], ['updatedOn' => 'DESC'], $perPage, $offset);
         $pagination  = new Pagination($page, $perPage);
 
         $seoManager = $this->get('seo_manager');
         $seoManager->setUserProfileRatingsSeoData();
 
-        return $this->render('@App/User/profile-ratings.html.twig', [
-            'books'       => $books,
-            'bookRatings' => $bookData,
-            'pagination'  => $pagination->paginate(count($bookIds)),
-            'show_author' => true,
+        return $this->render('@App/User/profile.html.twig', [
+            'view'           => 'ratings',
+            'books'          => $books,
+            'bookRatings'    => $bookData,
+            'pagination'     => $pagination->paginate(count($bookIds)),
+            'pagination_url' => '/user-profile/ratings/page/',
+            'show_author'    => true,
         ]);
     }
 
@@ -55,21 +57,24 @@ class UserController extends Controller
 
         $perPage     = $this->getParameter('default_per_page');
         $page        = $request->get('page', 1);
+        $offset      = ($page - 1) * $perPage;
         $pageService = $this->get('book_page_service');
         $bookData    = $pageService->getUserReviews($user->getId());
         $bookIds     = array_keys($bookData);
         $bookRepo    = $this->get('doctrine')->getRepository('AppBundle:Book');
-        $books       = $bookRepo->findBy(['id' => $bookIds], ['updatedOn' => 'DESC']);
+        $books       = $bookRepo->findBy(['id' => $bookIds], ['updatedOn' => 'DESC'], $perPage, $offset);
         $pagination  = new Pagination($page, $perPage);
 
         $seoManager = $this->get('seo_manager');
         $seoManager->setUserProfileReviewsSeoData();
 
-        return $this->render('@App/User/profile-reviews.html.twig', [
-            'books'       => $books,
-            'bookReviews' => $bookData,
-            'pagination'  => $pagination->paginate(count($bookIds)),
-            'show_author' => true,
+        return $this->render('@App/User/profile.html.twig', [
+            'view'           => 'reviews',
+            'books'          => $books,
+            'bookReviews'    => $bookData,
+            'pagination'     => $pagination->paginate(count($bookIds)),
+            'pagination_url' => '/user-profile/reviews/page/',
+            'show_author'    => true,
         ]);
     }
 }
