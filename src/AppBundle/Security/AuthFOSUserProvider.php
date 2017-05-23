f464e14d6654a63775ff29dd94f639a0357af5ea
Artyom Bochkarev <skivius@yandex.ru>
2017-05-23 22:45:27 +0300

    fixed social auth

--- src/AppBundle/Security/AuthFOSUserProvider.php
+++ src/AppBundle/Security/AuthFOSUserProvider.php
@@ -6,6 +6,7 @@ use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
 use HWI\Bundle\OAuthBundle\Security\Core\User\FOSUBUserProvider as BaseFOSUBProvider;
 use Symfony\Component\PropertyAccess\PropertyAccessor;
 use Symfony\Component\Security\Core\User\UserInterface;
+use FOS\UserBundle\Model\UserInterface as FOSUserInterface;
 
 class AuthFOSUserProvider extends BaseFOSUBProvider
 {
@@ -29,9 +30,11 @@ class AuthFOSUserProvider extends BaseFOSUBProvider
         $property = $this->getProperty($response);
         $username = $response->getUsername();
         $existingUser = $this->userManager->findUserBy([$property => $username]);
+
         if (null !== $existingUser) {
             $this->userManager->updateUser($existingUser);
         }
+
         $this->userManager->updateUser($user);
     }
 
@@ -40,44 +43,69 @@ class AuthFOSUserProvider extends BaseFOSUBProvider
      */
     public function loadUserByOAuthUserResponse(UserResponseInterface $response)
     {
-        $userEmail = $response->getEmail();
-        $property  = $this->getProperty($response);
-        $user      = $this->userManager->findUserBy([$property => $userEmail]);
-        if (null === $user) {
-            $user = $this->userManager->findUserByEmail($userEmail);
-            $providerSetter = 'set' . ucfirst($property);
-            $user->$providerSetter($userEmail);
-
-            $this->userManager->updateUser($user);
-        }
-
-        if (null === $user) {
-            $user = new User();
-            $userFirstName = $response->getFirstName();
-            $userLastName  = $response->getLastName();
-            $responseArray = $response->getResponse();
-            $userGender    = $responseArray['gender'];
-            $userPicture   = $response->getProfilePicture();
-            $user->setUsername($response->getRealName());
-            $user->setFirstName($userFirstName);
-            $user->setLastName($userLastName);
-            $user->setGender($userGender);
-            $user->setPicture($userPicture);
-            $user->setEmail($userEmail);
-            $user->setPassword('');
-            $user->setEnabled(true);
-
-            $providerSetter = 'set' . ucfirst($property);
-            $user->$providerSetter($userEmail);
-
-            $this->userManager->updateUser($user);
-
-            return $user;
-        }
-
+        $user = $this->prepareUser($response);
         $serviceName = $response->getResourceOwner()->getName();
         $setter = 'set' . ucfirst($serviceName) . 'AccessToken';
         $user->$setter($response->getAccessToken());
+
+        return $user;
+    }
+
+    /**
+     * @param UserResponseInterface $response
+     *
+     * @return User
+     */
+    protected function createUser(UserResponseInterface $response)
+    {
+        $user          = new User();
+        $userEmail     = $response->getEmail();
+        $property      = $this->getProperty($response);
+        $userFirstName = $response->getFirstName();
+        $userLastName  = $response->getLastName();
+        $responseArray = $response->getResponse();
+        $userGender    = $responseArray['gender'] ?? '';
+        $userPicture   = $response->getProfilePicture();
+        $user->setUsername($response->getRealName());
+        $user->setFirstName($userFirstName);
+        $user->setLastName($userLastName);
+        $user->setGender($userGender);
+        $user->setPicture($userPicture);
+        $user->setEmail($userEmail);
+        $user->setPassword('');
+        $user->setEnabled(true);
+
+        $providerSetter = 'set' . ucfirst($property);
+        $user->$providerSetter($userEmail);
+
+        $this->userManager->updateUser($user);
+
+        return $user;
+    }
+
+    /**
+     * @param UserResponseInterface $response
+     *
+     * @return FOSUserInterface|User
+     */
+    protected function prepareUser(UserResponseInterface $response)
+    {
+        $property  = $this->getProperty($response);
+        $userEmail = $response->getEmail();
+
+        if ($user = $this->userManager->findUserBy([$property => $userEmail])) {
+            return $user;
+        }
+
+        $user = $this->userManager->findUserByEmail($userEmail);
+
+        if ($user) {
+            $providerSetter = 'set' . ucfirst($property);
+            $user->$providerSetter($userEmail);
+            $this->userManager->updateUser($user);
+        } else {
+            $this->createUser($response);
+        }
 
         return $user;
     }
