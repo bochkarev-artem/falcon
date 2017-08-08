57dd5fe41a7dd83bb19b80d16499feb0fd4e43d1
Artyom Bochkarev <skivius@yandex.ru>
2017-08-08 08:41:09 +0300

    change sitemap creation command for update_books script

--- app/jobs/update_books.sh
+++ app/jobs/update_books.sh
@@ -4,6 +4,8 @@ php bin/console app:update-litres-data books stream
 php bin/console app:update-book-images
 php bin/console app:update-featured-menu
 php bin/console fos:elastica:populate --env=prod --no-debug
-php bin/console presta:sitemaps:dump web/sitemap.en/ --section=en --gzip --env=prod --no-debug
-php bin/console presta:sitemaps:dump web/sitemap.ru/ --section=ru --gzip --env=prod --no-debug
+rm -rf ./web/sitemap.en/sitemap.xml
+php bin/console presta:sitemaps:dump web/sitemap.en/ --section=en --base-url=http://bookary.eu/ --gzip --env=prod --no-debug
+rm -rf ./web/sitemap.ru/sitemap.xml
+php bin/console presta:sitemaps:dump web/sitemap.ru/ --section=ru --base-url=http://bookary.ru/ --gzip --env=prod --no-debug
 php bin/console cache:clear --env=prod --no-debug --no-warmup
