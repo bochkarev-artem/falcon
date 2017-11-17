f4f796a3710d94e8613f8fbb4de766e5799748fe
Artyom Bochkarev <artyom.bochkarev@justix.net>
2017-11-17 15:37:37 +0200

    add command to update images after litres update

--- app/jobs/update_books.sh
+++ app/jobs/update_books.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 cd /home/ubuntu/falcon/
 php bin/console app:update-litres-data books stream
+php bin/console app:update-book-images
 php bin/console app:update-featured-menu
 php bin/console cache:clear --env=prod --no-debug --no-warmup
 php bin/console app:schedule-book-index
