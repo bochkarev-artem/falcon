de9fad1d10eeb937d646618f0e74b9d020d0a0b5
Artyom Bochkarev <artyom.bochkarev@justix.net>
2017-12-04 16:59:14 +0200

    delete generation sitemap

--- app/jobs/update_books.sh
+++ app/jobs/update_books.sh
@@ -3,10 +3,5 @@ cd /home/ubuntu/falcon/
 php bin/console app:update-litres-data books stream
 sleep 10s
 php bin/console app:update-featured-menu
-sleep 10s
-php bin/console app:schedule-book-index --env=prod
 sleep 1m
-rm -rf ./web/sitemap.ru/sitemap.xml
-php bin/console presta:sitemaps:dump web/sitemap.ru/ --section=ru --base-url=http://bookary.ru/ --gzip --env=prod --no-debug
-rm -rf ./web/sitemap.en/sitemap.xml
-php bin/console presta:sitemaps:dump web/sitemap.en/ --section=en --base-url=http://bookary.eu/ --gzip --env=prod --no-debug
+php bin/console app:schedule-book-index --env=prod
