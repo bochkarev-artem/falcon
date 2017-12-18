7735cb791c7db84e3cb6ca2c4ec52232a0406dcb
Artyom Bochkarev <artyom.bochkarev@justix.net>
2017-12-18 23:42:51 +0200

    add sitemap generation

--- app/jobs/update_books.sh
+++ app/jobs/update_books.sh
@@ -5,3 +5,5 @@ sleep 10s
 php bin/console app:update-featured-menu
 sleep 1m
 php bin/console app:schedule-book-index --env=prod
+rm -rf ./web/sitemap/*
+php bin/console presta:sitemaps:dump web/sitemap/ --base-url=http://bookary.ru/ --gzip --env=prod --no-debug
