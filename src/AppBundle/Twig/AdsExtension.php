76144d9a4581bdbea59854a1924e0f9382179979
Artyom <artem9129@yandex.ru>
2019-09-01 18:03:48 +0200

    add links

--- src/AppBundle/Twig/AdsExtension.php
+++ src/AppBundle/Twig/AdsExtension.php
@@ -6,6 +6,7 @@
 namespace AppBundle\Twig;
 
 use AppBundle\Service\AdsManager;
+use AppBundle\Service\SAPE_client;
 
 class AdsExtension extends \Twig_Extension
 {
@@ -29,6 +30,7 @@ class AdsExtension extends \Twig_Extension
     {
         return [
             new \Twig_SimpleFunction('ad_by_position', [$this, 'getAdByPosition']),
+            new \Twig_SimpleFunction('init_sape', [$this, 'initSape']),
         ];
     }
 
@@ -41,6 +43,17 @@ class AdsExtension extends \Twig_Extension
     public function getAdByPosition($position, $showToAll = false)
     {
         return $this->adsManager->getAdByPosition($position, $showToAll);
+    }
+
+    public function initSape(int $number)
+    {
+        if (!defined('_SAPE_USER')){
+            define('_SAPE_USER', '840de354d9079d45da2f27ecfb3445f0');
+        }
+
+        $sape = new SAPE_client();
+
+        return $sape->return_links($number);
     }
 
     /**
